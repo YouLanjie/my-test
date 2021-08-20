d9f6698893f64f530ffa33e6bad604bb858c867f
youlanjie <ylj18926385312@163.com>
2021-08-20 08:36:35 +0800

    修正头文件的引用错误

--- Type_conversion.c
+++ Type_conversion.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "headfile/kbhit_input.h"
+#include "include/include.h"
 
 int main(int argc,char * argv[]) {
 	int a = 0x31,error = 0;
