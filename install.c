3195c8c31f3c33f04cadd068dc7a03fdb7537e6c
YouLanjie <ylj18926385312@163.com>
2022-07-03 15:07:24 +0800

    添加文件

---
+++ install.c
@@ -0,0 +1,200 @@
+#include "include/include.h"
+
+// #define USER               youlanjie
+// #define SHELL              /usr/bin/zsh
+// #define CONFIG             "install.conf"
+
+int backup();                   /* 备份配置文件 */
+int delete();                   /* 移除安装进度配置 */
+int install();                  /* 安装软件进行配置 */
+int readconfig(char *);         /* 读取文件信息 */
+int clean();                    /* 增加完成注释 */
+void help();                    /* 打印帮助信息 */
+
+char *CONFIG;
+
+int main(int argc, char * argv[]) {
+	int opt, stat = 0;
+
+	if (argc == 1) {
+		help();
+		return -1;
+	}
+	while ((opt = getopt(argc, argv, "hbirdf:")) != -1) {
+		switch (opt) {
+			case '?':
+			case 'h':
+				help();
+				return 0;
+				break;
+			case 'b':
+				stat = backup();
+				break;
+			case 'i':
+				stat = install();
+				break;
+			case 'd':
+			case 'r':
+				stat = delete();
+				break;
+			case 'f':
+				if (strcmp(optarg,"?") == 0) {
+					help();
+					return -1;
+				}
+				else {
+					CONFIG = optarg;
+				}
+				break;
+		}
+	}
+	return stat;
+}
+
+int backup() {
+	//system("echo \"backup:spacevim\"");
+	//if (access("/mnt/UserData/MyFiles/Backup/System/Arch/spacevim.tar.gz", 0) != 0) {
+	//	system("cd ~/ && tar -czf UserData/MyFiles/Backup/System/Arch/spacevim.tar.gz .SpaceVim .SpaceVim.d .cache/vimfiles/ .vim .config/nvim");
+	//}
+	//======================================
+	system("echo \"backup:chrome\"");
+	if (access("/mnt/UserData/MyFiles/Backup/System/Arch/chrome.tar.gz", 0) != 0) {
+		system("cd ~/ && tar -czf UserData/MyFiles/Backup/System/Arch/chrome.tar.gz .config/google-chrome .cache/google-chrome");
+	}
+	//======================================
+	system("echo \"backup:gpg\"");
+	if (access("/mnt/UserData/MyFiles/Backup/System/Arch/gpg.tar.gz", 0) != 0) {
+		system("cd ~/ && tar -czf UserData/MyFiles/Backup/System/Arch/gpg.tar.gz .ssh .gnupg");
+	}
+	//======================================
+	system("echo \"backup:config\"");
+	if (access("/mnt/UserData/MyFiles/Backup/System/Arch/config.tar.gz", 0) != 0) {
+		system("cd ~/ && tar -czf UserData/MyFiles/Backup/System/Arch/config.tar.gz .config/nvim .vim .config/fish .config/omf .local/share/omf .gitconfig .zshrc .oh-my-zsh .pam_environment .config/Kvantum .local/share/aurorae .local/share/plasma .config/i3 .config/picom .config/alacritty .config/polybar .local/share/fonts .config/ranger .config/gtk-2.0 .config/gtk-3.0 .config/gtk-4.0 .config/hmcl .config/kdeconnect .config/Kvantum .config/latte .config/obs-studio .config/uTools .local/share/color-schemes .config/rofi .config/conky");
+	}
+	//======================================
+	system("echo \"backup:root config\"");
+	if (access("/mnt/UserData/MyFiles/Backup/System/Arch/other.tar.gz", 0) != 0) {
+		system("cd / && tar -czf mnt/UserData/MyFiles/Backup/System/Arch/other.tar.gz etc/hosts etc/pacman.d/mirrorlist etc/pacman.conf");
+	}
+	system("echo \"backup:root config2\"");
+	if (access("/mnt/UserData/MyFiles/Backup/System/Arch/other2.tar.gz", 0) != 0) {
+		system("cd / && tar -czf mnt/UserData/MyFiles/Backup/System/Arch/other2.tar.gz usr/share/sddm/themes boot/grub/themes");
+	}
+	return 0;
+}
+
+int delete() {
+	char ch[100];
+	long size;
+	FILE *fp;
+
+	fp = fopen(CONFIG, "r+");
+	while (feof(fp) == 0) {
+		fgets(ch, 100, fp);
+		while ((ch[0] != '#' || ch[1] != '{') && feof(fp) == 0) {
+			size = ftell(fp);
+			fgets(ch, 100, fp);
+		}
+		if (feof(fp) != 0) {
+			return 0;
+		}
+		fseek(fp, size, 0);
+		fputs("{%", fp);
+	}
+	fclose(fp);
+	return 0;
+}
+
+int install() {
+	char command[1024];
+
+	while (1) {
+		switch (readconfig(command)) {
+			case -1:
+				printf("错误,文件异常\n");
+				return -1;
+				break;
+			case 1:
+				return 0;
+				break;
+		}
+		printf("command: %s\n", command);
+		// sleep(1);
+		if (system(command) != 0) {
+			printf("Command");
+			perror(command);
+			return -1;
+		}
+		else {
+			if (clean() == -1) {
+				return -1;
+			}
+		}
+	}
+	return 0;
+}
+
+int readconfig(char *command) {
+	char ch[100];
+	FILE *fp;
+
+	if (access(CONFIG, 0) == 0) {
+		fp = fopen(CONFIG, "r+");
+	}
+	else {
+		perror("CONFIG");
+		return -1;
+	}
+	if (!fp) {
+		perror("CONFIG");
+		return -1;
+	}
+	if (feof(fp) != 0) {
+		return 1;
+	}
+	fgets(ch, 100, fp);
+	while (ch[0] != '{' && feof(fp) == 0) fgets(ch, 100, fp);
+	if (feof(fp) != 0) {
+		return 1;
+	}
+	fgets(ch, 100, fp);
+	strcpy(command, ch);
+	fgets(ch, 100, fp);
+	while (ch[0] != '{' && ch[0] != '}' && feof(fp) == 0) {
+		if (ch[0] == '#' || ch[0] == '%') {
+			fgets(ch, 100, fp);
+			continue;
+		}
+		strcat(command, ch);
+		fgets(ch, 100, fp);
+	}
+	fclose(fp);
+	return 0;
+}
+
+int clean() {
+	char ch[100];
+	long size;
+	FILE *fp;
+
+	fp = fopen(CONFIG, "r+");
+	fgets(ch, 100, fp);
+	while (ch[0] != '{' && feof(fp) == 0) {
+		size = ftell(fp);
+		fgets(ch, 100, fp);
+	}
+	if (feof(fp) != 0) {
+		perror("Clean");
+		return -1;
+	}
+	fseek(fp, size, 0);
+	fputs("#{", fp);
+	fclose(fp);
+	return 0;
+}
+
+void help() {
+	printf("hello!\ninstall -b「备份」 -i「安装」 -[rd]「重新配置」 -f「指定配置文件」 -h「打印帮助信息并退出」\n");
+	return;
+}
+
