ad200a4cf7b6354764dadb65ae38956df06e2029
YouLanjie <ylj18926385312@163.com>
2023-03-31 21:01:38 +0800

    改进输入和输出

--- input_to_hex.c
+++ input_to_hex.c
@@ -1,10 +1,10 @@
 /*
  *   Copyright (C) 2023 YouLanjie
  *
- *   文件名称：b.c
+ *   文件名称：input_to_hex.c
  *   创 建 者：youlanjie
- *   创建日期：2023年01月25日
- *   描    述：
+ *   创建日期：2023年03月31日
+ *   描    述：将输入使用十六进制逐行打印
  *
  */
 
@@ -15,12 +15,12 @@ int main(void)
 {
 	char ch[10];
 	printf("请输入:\n");
-	scanf("%s", ch);
+	fgets(ch, 10, stdin);
 	for (int i = 0; i < 10; ++i) {
 		if (ch[i] < '!') {
-			printf("ch[%d]:0x%x\n", i, ch[i]);
+			printf("ch[%d]:\033[1;32m0x\033[1;33m%x\033[0m\n", i, ch[i]);
 		} else {
-			printf("ch[%d]:0x%x\t%c\n", i, ch[i], ch[i]);
+			printf("ch[%d]:\033[1;32m0x\033[1;33m%x\033[0m\t%c\n", i, ch[i], ch[i]);
 		}
 	}
 	return 0;
