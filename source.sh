ef06107ca553225721c5431dc68e1f9a659483b7
YouLanjie <ylj18926385312@163.com>
2023-05-26 10:05:47 +0800

    改动一些东西

--- source.sh
+++ source.sh
@@ -10,23 +10,51 @@
 #
 #================================================================
 
-bold="\033[1m"
-red="\033[31m"
-green="\033[32m"
-yellow="\033[33m"
-blue="\033[34m"
-clean="\033[0m"
+app_name="${0##*/}"
 
-line="$bold$red-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*$clean"
+F_B='\033[1m'
+F_I='\033[3m'
+F_U='\033[4m'
+F_S='\033[5m'
+F_R='\033[7m'
 
-Info() {
-    echo "$bold$blue" "Info:$yellow " "$1" "$clean"
+F_black='\033[30m'
+F_red='\033[31m'
+F_green='\033[32m'
+F_yellow='\033[33m'
+F_blue='\033[34m'
+F_purple='\033[35m'
+F_cyan='\033[36m'
+F_white='\033[37m'
+
+F_C='\033[0m'
+
+F_line="$bold$red-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*$clean"
+
+# Show an INFO message
+# $1: message string
+# $2: style string
+_msg_info() {
+    local _msg="${1}"
+    local _color="${2}"
+    [[ "${quiet}" == "y" ]] || printf "$F_B${F_green}[%s] INFO:$F_cyan$F_I$_color %s$F_C\n" "${app_name}" "${_msg}"
 }
 
-Info2() {
-    echo "$bold$blue" "Info:$green " "$1" "$clean"
+# Show a WARNING message
+# $1: message string
+_msg_warning() {
+    local _msg="${1}"
+    printf "$F_B${F_yellow}[%s] WARNING:$F_I %s$F_C\n" "${app_name}" "${_msg}" >&2
 }
 
-Warn() {
-    echo "$bold$yellow" "Waring: $1" "$clean"
+# Show an ERROR message then exit with status
+# $1: message string
+# $2: exit code number (with 0 does not exit)
+_msg_error() {
+    local _msg="${1}"
+    local _error=${2}
+    printf "$F_B${F_red}[%s] ERROR:$F_I %s$F_C\n"  "${app_name}" "${_msg}" >&2
+    if (( _error > 0 )); then
+        exit "${_error}"
+    fi
 }
