17629a5e67980e375bef98bea3dd0bd46bd2f3dc
YouLanjie <ylj18926385312@163.com>
2023-11-04 09:31:31 +0800

    初始化该分支，增加物理动画文件

---
+++ example.py
@@ -0,0 +1,156 @@
+#!/bin/python
+# A simple example file
+from manim import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+class mkCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+
+        square.next_to(circle, RIGHT, buff=0.5)
+        self.play(Create(square), Create(circle))
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+class OpeningManim(Scene):
+    def construct(self):
+        title = Tex(r"This is some \LaTeX")
+        basel = MathTex(r"\sum_{n=1}^\infty \frac{1}{n^2} = \frac{\pi^2}{6}")
+        VGroup(title, basel).arrange(DOWN)
+        self.play(
+            Write(title),
+            FadeIn(basel, shift=DOWN),
+        )
+        self.wait()
+
+        transform_title = Tex("That was a transform")
+        transform_title.to_corner(UP + LEFT)
+        self.play(
+            Transform(title, transform_title),
+            LaggedStart(*(FadeOut(obj, shift=DOWN) for obj in basel)),
+        )
+        self.wait()
+
+        grid = NumberPlane()
+        grid_title = Tex("This is a grid", font_size=72)
+        grid_title.move_to(transform_title)
+
+        self.add(grid, grid_title)  # Make sure title is on top of grid
+        self.play(
+            FadeOut(title),
+            FadeIn(grid_title, shift=UP),
+            Create(grid, run_time=3, lag_ratio=0.1),
+        )
+        self.wait()
+
+        grid_transform_title = Tex(
+            r"That was a non-linear function \\ applied to the grid",
+        )
+        grid_transform_title.move_to(grid_title, UL)
+        grid.prepare_for_nonlinear_transform()
+        self.play(
+            grid.animate.apply_function(
+                lambda p: p
+                + np.array(
+                    [
+                        np.sin(p[1]),
+                        np.sin(p[0]),
+                        0,
+                    ],
+                ),
+            ),
+            run_time=3,
+        )
+        self.wait()
+
+class test(Scene):
+    def construct(self):
+        text = Tex(r"TEXT")
+        self.play(Create(text))
+        self.wait()
+
+class test2(Scene):
+    def construct(self):
+        text = Tex(r"中文渲染测试", tex_template=TexTemplateLibrary.ctex)
+        text2 = Tex(r"这是第二段测试文字", tex_template=TexTemplateLibrary.ctex)
+        text2.move_to(DOWN)
+        self.play(Create(text))
+        self.play(FadeIn(text2, shift=UP))
+        self.wait()
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+
+class Shapes(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        circle.shift(LEFT)
+        square.shift(UP)
+        triangle.shift(RIGHT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+class MoreShapes(Scene):
+    # A few more simple shapes
+    # 2.7 version runs in 3.7 without any changes
+    # Note: I fixed my 'play command not found' issue by installing sox
+    def construct(self):
+        circle = Circle(color=PURPLE_A)
+        square = Square(fill_color=GOLD_B, fill_opacity=1, color=GOLD_A)
+        square.move_to(UP+LEFT)
+        circle.surround(square)
+        rectangle = Rectangle(height=2, width=3)
+        ellipse = Ellipse(width=3, height=1, color=RED)
+        ellipse.shift(2*DOWN+2*RIGHT)
+        pointer = CurvedArrow(2*RIGHT,5*RIGHT,color=MAROON_C)
+        arrow = Arrow(LEFT,UP)
+        arrow.next_to(circle,DOWN+LEFT)
+        rectangle.next_to(arrow,DOWN+LEFT)
+        ring = Annulus(inner_radius=.5, outer_radius=1, color=BLUE)
+        ring.next_to(ellipse, RIGHT)
+
+        self.add(pointer)
+        self.play(FadeIn(square))
+        self.play(Rotating(square),FadeIn(circle))
+        self.play(GrowArrow(arrow))
+        self.play(GrowFromCenter(rectangle), GrowFromCenter(ellipse), GrowFromCenter(ring))
+
