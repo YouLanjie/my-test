58ae77d2f13c53ffa1d4ca8b4f9e2ee2780be46f
YouLanjie <ylj18926385312@163.com>
2023-12-17 09:22:12 +0800

    增加`input.c`中字符串输入的功能

--- input.c
+++ input.c
@@ -90,17 +90,84 @@ int input_float_number(void)
 	return 0;
 }
 
+typedef struct t_inp
+{
+	int ch;
+	int id;
+	struct t_inp *n;
+	struct t_inp *l;
+} t_inp;
+
+t_inp *new_n()
+{
+	t_inp *new = malloc(sizeof(t_inp));
+	new->ch = 0;
+	new->id = 0;
+	new->n  = NULL;
+	new->l  = NULL;
+	return new;
+}
+
+void t_print(t_inp *tmp, int max)
+{
+	printf("\r[");
+	for (; tmp != NULL; tmp = tmp->n) {
+		if (tmp->id <= max-15) continue;
+		printf("\033[3m%c\033[0m", tmp->ch);
+	}
+	return;
+}
+
 int input_string(void)
 {
-	printf("String input test:\n");
-	printf("Waiting for editing\n");
+	printf("String input test(Only ASCII):\n");
+	printf("[_______________]\r[");
+	int count = 0;
+	int inp = 0;
+	t_inp *pH = new_n();
+	t_inp *pN = pH, *pL = NULL;
+	while (inp != '\r') {
+		t_print(pH, count);
+		inp = ctools_getch();
+		if ((inp > '!' && inp < '~') || inp == ' ') {
+			count++;
+			pN->id = count;
+			pN->ch = inp;
+			pN->n  = new_n();
+			pL = pN;
+			pN = pN->n;
+			pN->l  = pL;
+		} else if (inp == 0x7F && count > 0) {
+			count--;
+			pL = pL->l;
+			pN = pN->l;
+			free(pN->n);
+			pN->n = NULL;
+			pN->ch = 0;
+			printf("\b \b");
+		}
+	}
+	if (count == 0) return 0;
+	pN = pH;
+	char *least = malloc(sizeof(char)*(count + 1));
+	while (pN != NULL) {
+		least[pN->id - 1] = pN->ch;
+		pN = pN->n;
+	}
+	least[count] = '\0';
+	printf("\nFinal result:[\033[3m%s\033[0m]\n", least);
 	return 0;
 }
 
 int input_char(void)
 {
 	printf("Char input test:\n");
-	printf("Waiting for editing\n");
+	printf("[____]\r[");
+	int inp = 0;
+	inp = ctools_getch();
+	printf("\033[4m%c\033[0m\n", inp);
+	if (inp == 0x1B) printf("Final result:[] <0x1B>\n");
+	else printf("Final result:[%c] <0x%X>\n", inp, inp);
 	return 0;
 }
 
