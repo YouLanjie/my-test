70b631f10c26658e17dd2ca79c8aee5f1ef1a022
YouLanjie <ylj18926385312@163.com>
2024-01-27 16:57:26 +0800

    将两个脚本的功能合并到一起，增加脚本使用参数的功能

--- bilibili_build.sh
+++ bilibili_build.sh
@@ -1,20 +1,52 @@
 #!zsh
 
-list=$(find ./ -name "entry.json")
-# cmd=$(echo $list| sed "s/^/cat /"| sed 's/$/| sed "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/"/')
-#sed "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/"
 
-# echo $list
+help() {
+	echo "\
+usage: $1 [options]
+  options:
+     -i    show more info in output file
+     -h    show this message
+     -o    output file
+     -v    video output (3gp file)"
+	exit $2
+}
 
-rm -f run.sh
-echo $list|while read line
-do
-	ind=$(echo $line |sed "s/\.\/\([^/]*\/[^/]*\).*/\1/")
-	# echo "dir:$ind"
-	local title=""
-	title=$(cat "$line"|grep "download_subtitle" >/dev/null && cat "$line"|sed -n "s/.*\"download_subtitle\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
-	echo "# $title" >> run.sh
-	echo "ffmpeg -i \"`find $ind -name "audio.m4s"`\" \"$title.mp3\"" >> run.sh
-done
+running() {
+	list=$(find ./ -name "entry.json")
 
-# zsh -c "$cmd"
+	rm -f $f_o
+	echo $list|while read line
+	do
+		ind=$(echo $line |sed "s/\.\/\([^/]*\/[^/]*\).*/\1/")
+		# echo "dir:$ind"
+		local title=""
+		title=$(cat "$line"|grep "download_subtitle" >/dev/null && cat "$line"|sed -n "s/.*\"download_subtitle\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
+		if [[ $flag_info == "true" ]] {
+			echo "# $title <Dir: $ind>" >> $f_o
+		} else {
+			echo "# $title" >> $f_o
+		}
+		if [[ $flag_3gp == "true" ]] {
+			echo "ffmpeg -i \"`find $ind -name "video.m4s"`\" -i \"`find $ind -name "audio.m4s"`\" -r 12 -b:v 400k -s 352x288 -ab 12.2k -ac 1 -ar 8000 \"$title.3gp\"" >> $f_o
+		} else {
+			echo "ffmpeg -i \"`find $ind -name "audio.m4s"`\" \"$title.mp3\"" >> $f_o
+		}
+	done
+}
+
+flag_3gp="false"
+flag_info="false"
+f_o="run.sh"
+while {getopts "io:vh" OPTION} {
+	case $OPTION {
+		i) flag_info="true" ;;
+		o) f_o=$OPTARG ;;
+		v) flag_3gp="true" ;;
+		h|?) help $0 ;;
+		*) help $0 -1 ;;
+	}
+}
+
+running
+
