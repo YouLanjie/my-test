6086f554d8195aa9eb02f475724dd15c0956451a
YouLanjie <ylj18926385312@163.com>
2024-01-27 15:56:44 +0800

    修复脚本无法区分分P的问题

--- bilibili_build.sh
+++ bilibili_build.sh
@@ -9,9 +9,10 @@ list=$(find ./ -name "entry.json")
 rm -f run.sh
 echo $list|while read line
 do
-	ind=$(echo $line |sed "s/\.\/\([^/]*\).*/\1/")
+	ind=$(echo $line |sed "s/\.\/\([^/]*\/[^/]*\).*/\1/")
 	# echo "dir:$ind"
-	title=$(cat "$line"|sed "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/")
+	local title=""
+	title=$(cat "$line"|grep "download_subtitle" >/dev/null && cat "$line"|sed -n "s/.*\"download_subtitle\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
 	echo "# $title" >> run.sh
 	echo "ffmpeg -i \"`find $ind -name "audio.m4s"`\" \"$title.mp3\"" >> run.sh
 done
