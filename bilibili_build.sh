2aafb96c10133ba1f0cc89c517f4ae3651ff3ce3
YouLanjie <ylj18926385312@163.com>
2024-02-18 19:37:47 +0800

    添加将视频文件和音频文件合并的功能

--- bilibili_build.sh
+++ bilibili_build.sh
@@ -8,7 +8,8 @@ usage: $1 [options]
      -i    show more info in output file
      -h    show this message
      -o    output file
-     -v    video output (3gp file)"
+     -v    video output (3gp file)
+     -V    video output (mp4 file)"
 	exit $2
 }
 
@@ -21,29 +22,42 @@ running() {
 		ind=$(echo $line |sed "s/\.\/\([^/]*\/[^/]*\).*/\1/")
 		# echo "dir:$ind"
 		local title=""
-		title=$(cat "$line"|grep "download_subtitle" >/dev/null && cat "$line"|sed -n "s/.*\"download_subtitle\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
-		[[ $title == "" ]] && title=$(cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
+
+		local subtitle=""
+		subtitle=$(cat "$line"|grep "download_subtitle" >/dev/null && cat "$line"|sed -n "s/.*\"download_subtitle\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
+		[[ $subtitle != "" ]] && title=$subtitle
+		[[ $subtitle == "" ]] && title=$(cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
+
+		local ind_title=""
+		local index=""
+		ind_title=$(cat "$line"|grep "index_title" >/dev/null && cat "$line"|sed -n "s/.*\"index_title\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"title\":\"\\([^\"]*\\)\".*/\\1/p")
+		index=$(cat "$line"|grep "index" >/dev/null && cat "$line"|sed -n "s/.*\"index\":\"\\([^\"]*\\)\".*/\\1/p" || cat "$line"|sed -n "s/.*\"index\":\"\\([^\"]*\\)\".*/\\1/p")
+		[[ $ind_title != "" ]] && title="${title}_No${index}_${ind_title}"
+
 		if [[ $flag_info == "true" ]] {
 			echo "# $title <Dir: $ind>" >> $f_o
 		} else {
 			echo "# $title" >> $f_o
 		}
-		if [[ $flag_3gp == "true" ]] {
+		if [[ $flag_type == "3gp" ]] {
 			echo "ffmpeg -i \"`find $ind -name "video.m4s"`\" -i \"`find $ind -name "audio.m4s"`\" -r 12 -b:v 400k -s 352x288 -ab 12.2k -ac 1 -ar 8000 \"$title.3gp\"" >> $f_o
+		} elif [[ $flag_type == "mp4" ]] {
+			echo "ffmpeg -i \"`find $ind -name "audio.m4s"`\" -c copy \"$title.mp4\"" >> $f_o
 		} else {
 			echo "ffmpeg -i \"`find $ind -name "audio.m4s"`\" \"$title.mp3\"" >> $f_o
 		}
 	done
 }
 
-flag_3gp="false"
+flag_type="mp3"
 flag_info="false"
 f_o="run.sh"
-while {getopts "io:vh" OPTION} {
+while {getopts "io:vVh" OPTION} {
 	case $OPTION {
 		i) flag_info="true" ;;
 		o) f_o=$OPTARG ;;
-		v) flag_3gp="true" ;;
+		v) flag_type="3gp" ;;
+		V) flag_type="mp4" ;;
 		h|?) help $0 ;;
 		*) help $0 -1 ;;
 	}
