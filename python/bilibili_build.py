571172657622ea518534ac0b37db3beaa45ee56d
YouLanjie <ylj18926385312@163.com>
2025-05-17 15:52:25 +0800

    TUI支持多选视频，支持修改总标题格式，为输出文件名增加替换特殊字符功能，修改帮助

--- python/bilibili_build.py
+++ python/bilibili_build.py
@@ -12,9 +12,11 @@ import getopt
 import json
 
 class Video():
+    """存储单个视频信息"""
     def __init__(self, file):
         if file.is_file() is False:
-            return None
+            print(f"`{str(file)}`不是文件")
+            sys.exit(1)
         self.timestamp = file.stat().st_mtime
         self.file = str(file)
         # 针对分p视频的单个视频
@@ -40,27 +42,37 @@ class Video():
         if self.index is None:
             self.index = 0
         # set final title
-        self.title = self.maintitle
-        if self.part not in ("None", "", self.maintitle):
-            self.title = f"{self.title} - {self.part}"
-        if self.subtitle not in ("None", "", self.maintitle, f"{self.maintitle} {self.part}"):
-            self.title = f"{self.title} - {self.subtitle}"
+        self.reset_title()
     def getlist(self):
+        """返回视频的信息列表"""
         return [self.owner, self.title, "AV"+self.avid, self.bvid, self.dir_self]
+    def reset_title(self, mode="auto"):
+        """根据给出模式重新设置总标题"""
+        if mode == "maintitle":
+            self.title = self.maintitle
+        elif mode == "part":
+            self.title = self.part
+        else:
+            self.title = self.maintitle
+            if self.part not in ("None", "", self.maintitle):
+                self.title = f"{self.title} - {self.part}"
+            if self.subtitle not in ("None", "", self.maintitle, f"{self.maintitle} {self.part}"):
+                self.title = f"{self.title} - {self.subtitle}"
 
 class Ui():
+    """TUI,需要curses"""
     width=os.get_terminal_size().columns
     position=0
     select=0
+    collection = []
     def __init__(self, stdscr, content : list) -> None:
+        if "curses" not in sys.modules:
+            print("模块curses未导入，curses界面不可用")
+            sys.exit(1)
         info_height = 9
         self.height=os.get_terminal_size().lines - info_height - 1
         self.stdscr = stdscr
-        # self.stdscr = curses.initscr()
-        # curses.noecho()
-        # curses.cbreak()
         curses.curs_set(0)
-        # curses.start_color()
         curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
         curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -69,6 +81,7 @@ class Ui():
         self.content = content
         # self.height = len(self.content)
     def gsl(self, s, lim, start=0):
+        """根据打印宽度截断字符串"""
         width = 0
         count = 0
         for c in s[start:]:
@@ -82,20 +95,25 @@ class Ui():
                 break
         return s[start:count]
     def print_list(self, content):
+        """打印视频列表"""
         self.list.clear()
         for i in range(self.position, self.position+self.height):
             if i >= len(content):
                 break
             p = content[i]
             width = self.width - 2
-            color = None
-            if type(p) is list:
+            color = 0
+            if isinstance(p, list):
                 title = self.gsl((p[0].title if len(p)==1 else p[0].maintitle)+" "*width, width)
-                color = curses.color_pair(2) if len(p) != 1 else None
+                if len(p) != 1:
+                    color+=curses.color_pair(2)
             else:
                 title = self.gsl(p.title+" "*width, width)
+            if i in self.collection:
+                color+=curses.A_BOLD
             if i == self.select:
-                self.list.addstr(f"> {title}\n", curses.color_pair(1))
+                color+=curses.A_UNDERLINE
+                self.list.addstr(f"> {title}\n", color)
                 continue
             if color:
                 self.list.addstr(f"  {title}\n", color)
@@ -104,9 +122,10 @@ class Ui():
             # print(p.getlist())
         self.list.refresh()
     def print_info(self, content):
+        """打印光标处视频详细信息"""
         p = content[self.select]
         sets = True
-        if type(p) is list:
+        if isinstance(p, list):
             sets = len(p) == 1
             p = p[0]
         self.info.clear()
@@ -123,8 +142,7 @@ class Ui():
             )
         self.info.refresh()
     def check(self, content):
-        # if self.height > len(self.content):
-            # self.height = len(self.content)
+        """越界、上下滑动检查"""
         if self.select < 0:
             self.select = len(content)-1
         elif self.select >= len(content):
@@ -134,26 +152,28 @@ class Ui():
         while self.position > self.select:
             self.position -= 1
     def cmd(self, cmd:str):
+        """暂时脱离curses环境执行命令"""
         curses.def_prog_mode()
         curses.endwin()
         os.system(cmd)
         curses.reset_prog_mode()
     def main(self):
+        """ui主程序"""
         self.check(self.content)
-        # print("hit 'q' to quit,jk to move")
         self.stdscr.refresh()
-        # curses.endwin()
-        # return None
         inp = "0"
         deep = 0
         position = 0
         select = 0
+        collection = []
         while inp != "q":
             cont = [self.content, self.content[select]][deep]
-            obj = cont[self.select]
+            obj = [cont[self.select]] \
+                    if len(self.collection) == 0 else \
+                    [cont[i] for i in self.collection]
             self.print_list(cont)
             self.print_info(cont)
-            inp = "%c" % self.stdscr.getch()
+            inp = chr(self.stdscr.getch())
             if inp == "j":
                 self.select+=1
             elif inp == "k":
@@ -164,26 +184,47 @@ class Ui():
                 self.select=-1
             elif inp == "l" and len(self.content[self.select]) != 1 and deep == 0:
                 position,select,self.position,self.select = self.position, self.select, 0, 0
+                collection, self.collection = self.collection, []
                 deep = 1
             elif inp == "h" and deep == 1:
                 self.position,self.select = position,select
+                self.collection = collection
                 deep = 0
             elif inp == "r":
                 self.select = len(cont) - self.select - 1
                 cont.reverse()
             elif inp == "p":
-                self.cmd(cmd_genal([obj], "play"))
+                self.cmd(cmd_genal(obj, "play"))
             elif inp == "c":
-                self.cmd(cmd_genal([obj], "copy"))
+                self.cmd(cmd_genal(obj, "copy"))
             elif inp == "e":
-                self.cmd(cmd_genal([obj], "mp3"))
+                self.cmd(cmd_genal(obj, "mp3"))
             elif inp == "E":
-                self.cmd(cmd_genal([obj], "mp4"))
+                self.cmd(cmd_genal(obj, "mp4"))
+            elif inp == " ":
+                if self.select in self.collection:
+                    self.collection.remove(self.select)
+                else:
+                    self.collection.append(self.select)
+                self.select+=1
+            elif inp == "A":
+                self.collection = []
+            elif inp == "a":
+                self.collection = list(range(len(cont)))
+            elif inp in ("1", "2", "3"):
+                if not isinstance(obj[0], list):
+                    obj = [obj]
+                title_mode = {"1":"maintitle", "2":"part", "3":"auto"}
+                for i in obj:
+                    for j in i:
+                        j.reset_title(title_mode[inp])
+            elif inp == "4":
+                cfg["vfat_name"] = not cfg["vfat_name"]
             self.check([self.content, self.content[select]][deep])
         # curses.endwin()
-        return None
 
 def cmd_genal(li, mode) -> str:
+    """生成shell命令用于保存或执行"""
     t = ""
     if type(li[0]) != list:
         li = [li]
@@ -198,45 +239,64 @@ def cmd_genal(li, mode) -> str:
             if len(vid) == 0 or len(aud) == 0:
                 t+=f"# {j.dir_self}:no media file({j.title})"
                 continue
+            output = f"{cfg["outputd"]+j.title}"
             t+=f"# {j.title if len(i) == 1 else j.part} <Dir: {j.dir_self}>\n"
             if mode == "3gp":
                 t+=f"ffmpeg -i \"{str(vid[0])}\" -i \"{str(aud[0])}\""\
-                    +" -r 12 -b:v 400k -s 352x288 -ab 12.2k -ac 1 -ar 8000 -c copy"\
-                    +f" \"{cfg["outputd"]+j.title}.3gp\"\n"
+                    +" -r 12 -b:v 400k -s 352x288 -ab 12.2k -ac 1 -ar 8000 -c copy "
+                output+=".3gp"
             elif mode == "mp4":
-                t+=f"ffmpeg -i \"{str(vid[0])}\" -i \"{str(aud[0])}\""\
-                    +f" -c copy \"{cfg["outputd"]+j.title}.mp4\"\n"
+                t+=f"ffmpeg -i \"{str(vid[0])}\" -i \"{str(aud[0])}\" -c copy "
+                output+=".mp4"
             elif mode in ("m4a" "copy"):
-                t+=f"cp \"{str(aud[0])}\" \"{cfg["outputd"]+j.title}.m4a\"\n"
+                t+=f"cp \"{str(aud[0])}\" "
+                output+=".m4a"
             elif mode == "play":
                 t+=f"echo \"Now Playing:{j.title}\"\n{cfg["player"]} \"{str(aud[0])}\"\n"
+                continue
             else:
-                t+=f"ffmpeg -i \"{str(aud[0])}\" \"{cfg["outputd"]+j.title}.mp3\"\n"
+                t+=f"ffmpeg -i \"{str(aud[0])}\" "
+                output+=".mp3"
+            # 替换vfat系统不允许出现的字符（包括部分正常文件系统特殊字符）
+            replacement = ["/", "／"]
+            if cfg["vfat_name"]:
+                replacement = ["\\/:*?\"<>", "＼／∶＊？＂〈〉｜"]
+            for k,l in zip(list(replacement[0]), list(replacement[1])):
+                if not cfg["vfat_name"]:
+                    break
+                output=output.replace(k, l)
+            if output[0] == "-":
+                output = "./"+output
+            t+=f"\"{output}\"\n"
         if len(i) != 1:
             t+=f"# END OF SETS: {i[0].maintitle} <AV{i[0].avid}>\n"
             t+="# =========================\n\n"
     t+="echo \"Done!\\n\"\n"
     return t
 
-def main(argv):
+def run_main(argv):
+    """运行主程序(需要argv)"""
     list_mode = False
     inputd = "./"
     outputf = None
     mode = "mp3"
     try:
-        opts, args = getopt.getopt(argv,
+        opts = getopt.getopt(argv,
             "hli:o:O:m:p:",
-            ["help", "list", "input-dir=", "output-dir=",
-             "output-file=","mode=", "player="]
+            ["help", "list", "no-vfat-name", "input-dir=",
+             "output-dir=", "output-file=","mode=",
+             "player="]
         )
     except getopt.GetoptError:
         mhelp(1, msg="[!] Err: getopt error")
-        exit(-1)
-    for option, argument in opts:
+        sys.exit(-1)
+    for option, argument in opts[0]:
         if option in ("-h", "--help"):
             mhelp()
         elif option in ("-l", "--list"):
             list_mode = True
+        elif option in ("-n", "--no-vfat-name"):
+            cfg["vfat_name"]=False
         elif option in ("-m", "--mode"):
             mode = argument
         elif option in ("-i", "--input-dir"):
@@ -253,8 +313,8 @@ def main(argv):
         mhelp(msg="[!] no input file was found")
     print(f"{len(input_f)} files were found")
     li = []
-    for i in range(len(input_f)):
-        v = Video(input_f[i])
+    for i in enumerate(input_f):
+        v = Video(input_f[i[0]])
         if len(li) > 0 and li[-1][0].dir_parent == v.dir_parent:
             li[-1].append(v)
         else:
@@ -267,35 +327,41 @@ def main(argv):
             for j in i:
                 print(j.getlist(), end=" ")
             print()
-        exit(0)
+        sys.exit(0)
     if outputf is not None:
         file = open(outputf, "w")
         file.write(cmd_genal(li, mode))
         file.close()
-        exit(0)
+        sys.exit(0)
     return li
 
 def mhelp(ret=0, msg=""):
+    """显示帮助信息"""
     if msg != "":
         print(str(msg))
     print("Usage bilibili_build.py [OPTION]")
     print("OPTION:")
-    print("    -h         --help           show this help")
-    print("    -l         --list           list media")
-    print("    -i <dir>   --input-dir=dir  set output file")
-    print("    -o <dir>   --output-dir=dir set output file")
-    print("    -O <file>  --output=file    set output file")
-    print("    -p <cmd>   --player=cmd     set default player")
-    print("    -m <mode>  --mode=mode      set output format")
-    print("[*] mode list:mp3(default)|mp4|m4a(copy)|3gp|play")
-    print("[*] make sure the file `entry.json` exist")
-    print("[*] in the tui,press `hjkl` to move,`eE` to export media")
-    print("    `p` to play,`c` to copy audio file")
-    exit(ret)
+    print("    -h         --help           显示帮助")
+    print("    -l         --list           列出视频")
+    print("    -n         --no-vfat-name   不替换特殊字符")
+    print("    -i <dir>   --input-dir=dir  设置输入文件夹")
+    print("    -o <dir>   --output-dir=dir 设置输出文件夹")
+    print("    -O <file>  --output=file    设置输出文件")
+    print("    -p <cmd>   --player=cmd     设置默认播放器")
+    print("    -m <mode>  --mode=mode      设置脚本输出类型")
+    print("[*] 输出类型:mp3(默认)|mp4|m4a(仅复制)|3gp|play")
+    print("[*] 请确保`entry.json`文件存在")
+    print("[*] 在TUI中`jk`上下移动 `l`查看多p视频 `h`返回")
+    print("    `gG`快速跳转到列表头部或尾部 `r`反转列表排序")
+    print("    ` `选中或取消选中光标项 `aA`全选或取消全选")
+    print("    `e`导出为mp3 `E`导出为mp4 `c`复制出m4a文件")
+    print("    `p`播放音频文件 `123`切换总标题命名格式")
+    print("    `4`切换是否替换输出文件名特殊字符")
+    sys.exit(ret)
 
-cfg = {"player":"mpv", "outputd":""}
+cfg = {"player":"mpv", "outputd":"", "vfat_name":True}
 if __name__ == "__main__":
-    video_list = main(sys.argv[1:])
+    video_list = run_main(sys.argv[1:])
     video_list.reverse()
     import curses
     curses.wrapper(lambda stdscr: Ui(stdscr, video_list).main())
