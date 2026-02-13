f8ad47050a5c626c0bc203d3be8fb90531d5e762
YouLanjie <ylj18926385312@163.com>
2026-02-13 23:05:10 +0800

    update

--- src_c/ALSA_str_read.c
+++ src_c/ALSA_str_read.c
@@ -11,10 +11,10 @@
 
 #include "tools.h"
 #include <limits.h>
+#include <stdint.h>
 
 #define BUFFSIZE 2048
-#define SCR_HEIGHT 8
-#define SCR_WIDTH 32
+#define SCR_HEIGHT 4
 
 #define NFLG_LEFT (1 << 0)
 #define NFLG_RIGHT (1 << 1)
@@ -26,10 +26,13 @@ unsigned int SAMPLE_RATE = 44100;
 struct Note {
 	double freq;
 	double amplitude;
-	double fade;
-	unsigned long duration;
-	int flag;
-	short *pwav;
+	double   type;		/* type分音符 */
+	uint8_t  notes;		/* 以notes分音符为一拍 */
+	uint8_t  beates;	/* 每小节beates拍 */
+	uint8_t  speed;		/* 速度，n拍/分钟 */
+	uint8_t  track;
+	uint8_t  flag;		/* NFLG_* flags */
+	uint16_t *pwav;
 	struct Note *pNext;
 };
 
@@ -37,27 +40,40 @@ struct Note *create_note(double freq)
 {
 	struct Note *p = malloc(sizeof(struct Note));
 	if (!p) return NULL;
-	*p = (struct Note){freq, 0.5, 0, INT_MAX,
-		NFLG_LEFT|NFLG_RIGHT, NULL, NULL};
+	*p = (struct Note){
+		freq, .amplitude=0.5,
+		.type=4, .notes=4, .beates=4,
+		.speed=120, .track=0,
+		.flag=NFLG_LEFT|NFLG_RIGHT,
+		.pwav=NULL, NULL
+	};
 	return p;
 }
 
-char *i2b(long data, int len)
+char *i2b(unsigned long data, int bytes, char *buf, size_t buf_size)
 {
-	static char s[41] = "";
-	memset(s, 0, 41);
-	for (int i = 0; i < len*8; ++i) {
-		s[i] = data & (1 << (len*8-i-1)) ? '1' : '0';
+	if (!buf || buf_size == 0) return NULL;
+	if (bytes <= 0 || bytes > sizeof(data)) bytes = sizeof(data);
+
+	int bits = bytes * 8;
+	if (bits >= buf_size) bits = buf_size - 1;  // 留1字节给'\0'
+
+	for (int i = 0; i < bits; ++i) {
+		unsigned long mask = 1UL << (bits - 1 - i);  // 1UL 保证至少 32/64 位
+		buf[i] = (data & mask) ? '1' : '0';
 	}
-	return s;
+	buf[bits] = '\0';
+	return buf;
 }
 
-int main(int argc, char *argv[])
+struct Note *parse_notes()
 {
+	char istty = 0;
 	int (*input_func)() = getchar;
 	if (isatty(STDIN_FILENO)) {
 		printf("stdin是tty,改用_getch()\n");
 		input_func = _getch;
+		istty = 1;
 	}
 	static const double note_freq[256] = { 0,
 		['c'] = 130.8, ['d'] = 146.8, ['e'] = 164.8, ['f'] = 174.6,
@@ -68,40 +84,42 @@ int main(int argc, char *argv[])
 		['5'] = 784.0, ['6'] = 880.0, ['7'] = 987.8, ['0'] = -1,
 		['{'] = 20, ['}'] = 20000};
 	char scr[BUFSIZ] = "",
+	     scr2[BUFSIZ] = "",
+	     buf[30] = "",
 	     key[40] = "",
 	     value[40] = "";
-	unsigned short setting_mode = 0;
-	int type = 4, base = 4, speed = 120,
-	    track = 0, c = 0;
+	double fade = 0;
+	uint8_t track = 0, setting_mode = 0;
+	int beates = 4, /* 每小节type拍(重置计数器用) */
+	    notes = 4,  /* 以几分音符为一拍 */
+	    type = 4,   /* 默认type分音符 */
+	    speed = 120,
+	    c = 0;
 	struct Note *pH = NULL, *p = NULL;
-	for (int i = 0; i < SCR_HEIGHT; ++i) printf("\n");
+	if (istty) for (int i = 0; i < SCR_HEIGHT; ++i) printf("\n");
 	while (c != EOF) {
-		if (p) {
+		if (istty) {
+			if (p) {
+				sprintf(scr2,
+					"[%d/%d %dbpm:%d] 1/%.2lf %5.1lfHz (%0.4lf%%) [%s]\n",
+					p->notes,p->beates,p->speed,p->track,p->type,
+					p->freq, p->amplitude, i2b(p->flag, 1, buf, 30));
+			} else sprintf(scr2, "No Note was created.\n");
 			sprintf(scr,
-				"Typ:%d;Base:%d;SPD:%d;TRAC:%d\n"
-				"KEY: '%s'  VALUE: '%s'\n"
-				"Freq: %5.1lf  Amp: %lf\nduration: %ld (%d)\n"
-				"fadeout: %lf\nLEGATO: %d\nPORTAMENTO: %d\n"
-				"LR: %d,%d (flg:%s)",
-				type, base, speed, track, key, value,
-				p->freq, p->amplitude, p->duration,
-				(int)(INT_MAX/(p->duration?p->duration:INT_MAX)),
-				p->fade, p->flag&NFLG_LEGATO, p->flag&NFLG_PORTAMENTO,
-				p->flag&NFLG_LEFT, p->flag&NFLG_RIGHT, i2b(p->flag, 2));
-		} else sprintf(scr,
-			       "Typ: %d; Base: %d; SPD:%d\n"
-				"KEY: '%s'  VALUE: '%s'\n"
-				"No Note was created.\n",
-				type, base, speed, key, value);
-		print_in_box(scr, 0, get_winsize_row()-SCR_HEIGHT,
-			     SCR_WIDTH, SCR_HEIGHT, 0, 0, NULL, 0);
+				"[%d/%d %dbpm:%d] 1/%d  || fade:%lf\n"
+				"KEY: '%s' VALUE: '%s'\n%s",
+				notes,beates,speed,track,type, fade,
+				key, value, scr2);
+			print_in_box(scr, 0, get_winsize_row()-SCR_HEIGHT,
+				     -1, SCR_HEIGHT, 0, 0, NULL, 0);
+		}
 
 		c=input_func();
 		if (c==EOF) break;
 		if (c >= 256 || c < 0) continue;
 
 		if (setting_mode == 0) {
-			if (c == 'q') c = EOF;
+			if (istty && c == 'q') break;
 		} else if (setting_mode <= 40) {
 			if (c != '=') key[setting_mode-1] = c;
 			else setting_mode = 40;
@@ -111,10 +129,12 @@ int main(int argc, char *argv[])
 			if (c != ';') value[setting_mode-41] = c;
 			else {
 				setting_mode = 0;
-#define ifin(str, var, check) if (strcmp(key, str) == 0) {sscanf(value, "%d", &var); check;} else
-				ifin("track", track, )
-				ifin("base", base, if(base%4!=0||base<1) base=4)
-				ifin("type", type, if(type<1)type=4);
+#define ifin(str, fmt, var, check) if (strcmp(key, str) == 0) {sscanf(value, fmt, &var); check;} else
+				ifin("track", "%hhd", track, )
+				ifin("speed", "%d", speed, if(speed<1) speed=120)
+				ifin("beates", "%d", beates, if(beates<1) )
+				ifin("notes", "%d", notes, if(notes%4!=0||notes<1) notes=4)
+				ifin("type", "%d", type, if(type%4!=0||type<1)type=4);
 #undef ifin
 				memset(key, 0, 40);
 				memset(value, 0, 40);
@@ -129,10 +149,13 @@ int main(int argc, char *argv[])
 		if (note_freq[c]) {
 			struct Note *p2 = p;
 			p = create_note(note_freq[c]);
-			p->duration = SAMPLE_RATE*(base/type)*(60/speed);
+			/*p->duration = SAMPLE_RATE*((double)base/4)*((double)60/speed);*/
+			p->type = type;
+			p->track = track;
+			if (!pH) pH = p;
 			if (p2) {
-				if (!pH) pH = p2;
 				p2->pNext = p;
+				p->amplitude = p2->amplitude*(1.0+fade);
 			}
 		} else if (c == ':') {
 			setting_mode = 1;
@@ -145,18 +168,18 @@ int main(int argc, char *argv[])
 		case '\r':
 		case '\n':
 		case '\t':
-			if (p) free(p);
-			p = NULL;
+			/*if (p) free(p);*/
+			/*p = NULL;*/
 			c = 0;
 			break;
 		case '/':
-			p->duration /= 2;
+			p->type *= 2;
 			break;
 		case '*':
-			p->duration *= 2;
+			p->type /= 2;
 			break;
 		case '.':
-			p->duration *= (3.0/2.0);
+			p->type /= (3.0/2.0);
 			break;
 		case '~':
 			p->flag ^= NFLG_LEGATO;
@@ -189,13 +212,13 @@ int main(int argc, char *argv[])
 			p->flag ^= NFLG_RIGHT;
 			break;
 		case '<':
-			p->fade += 0.005;
+			fade += 0.005;
 			break;
 		case '>':
-			p->fade -= 0.005;
+			fade -= 0.005;
 			break;
 		case '=':
-			p->fade = 0;
+			fade = 0;
 			break;
 		case ':':
 			setting_mode = 1;
@@ -205,22 +228,29 @@ int main(int argc, char *argv[])
 			break;
 		}
 	}
-	printf("\e[%d;%dH\n====Quit====\n", get_winsize_row()-1, 0);
+	if (istty)printf("\e[%d;%dH\n====Quit====\n", get_winsize_row()-1, 0);
+	return pH;
+}
 
-	p = pH;
-	struct Note *p2 = p;
+int main(int argc, char *argv[])
+{
+
+	int count = 0;
+	struct Note *pH = NULL,
+		    *p = NULL,
+		    *p2 = NULL;
+	char buf[20] = "";
+	p2 = p = pH = parse_notes();
 	while (p) {
-		printf("Freq: %5.1lf  Amp: %lf\nduration: %ld (%d)\n"
-		       "fadeout: %lf\nLEGATO: %d\nPORTAMENTO: %d\n"
-		       "LR: %d,%d (flg:%s)\n=============\n",
-		       p->freq, p->amplitude, p->duration,
-		       (int)(INT_MAX/(p->duration?p->duration:INT_MAX)),
-		       p->fade, p->flag&NFLG_LEGATO, p->flag&NFLG_PORTAMENTO,
-		       p->flag&NFLG_LEFT, p->flag&NFLG_RIGHT, i2b(p->flag, 2));
+		printf("[%d/%d %dbpm:%d] 1/%-3.2lg %5.1lfHz (%0.4lf%%) [%s]\n",
+		       p->notes,p->beates,p->speed,p->track,p->type,
+		       p->freq, p->amplitude, i2b(p->flag, 1, buf, 30));
 		p = p->pNext;
+		count++;
 		free(p2);
 		p2 = p;
 	}
+	printf("Count: %d\n", count);
 	return 0;
 }
 
--- src_c/input_to_hex.c
+++ src_c/input_to_hex.c
@@ -11,14 +11,33 @@
 
 #include "tools.h"
 
-char *i2b(long data, int len)
+char *i2b(unsigned long data, int bytes, char *buf, size_t buf_size)
 {
-	static char s[41] = "";
-	memset(s, 0, 41);
-	for (int i = 0; i < len*8; ++i) {
-		s[i] = data & (1 << (len*8-i-1)) ? '1' : '0';
+	if (!buf || buf_size == 0) return NULL;
+	if (bytes <= 0 || bytes > sizeof(data)) bytes = sizeof(data);
+
+	int bits = bytes * 8;
+	if (bits >= buf_size) bits = buf_size - 1;  // 留1字节给'\0'
+
+	for (int i = 0; i < bits; ++i) {
+		unsigned long mask = 1UL << (bits - 1 - i);  // 1UL 保证至少 32/64 位
+		buf[i] = (data & mask) ? '1' : '0';
 	}
-	return s;
+	buf[bits] = '\0';
+	return buf;
+}
+
+int print_bits(int ind, long data, int len)
+{
+	char buf[65] = "";
+	printf("ch[\033[1;33m%3d\033[0m]: "
+	       "\033[1;32m0x\033[1;33m%02lx\033[0m: "
+	       "\033[1;32m0b\033[1;33m%s\033[0m",
+	       ind, data, i2b(data, len, buf, 65));
+	if (data >= '!' && data < CHAR_MAX)
+		printf("\t%c\n", (char)data);
+	else printf("\n");
+	return 0;
 }
 
 int main(void)
@@ -28,15 +47,7 @@ int main(void)
 	printf("请输入:\n");
 	fgets(ch, len, stdin);
 	for (int i = 0; i < len && ch[i] != 0; ++i) {
-		printf("ch[\033[1;33m%3d\033[0m]: "
-		       "\033[1;32m0x\033[1;33m%02x\033[0m: "
-		       "\033[1;32m0b\033[1;33m%s\033[0m",
-		       i, ch[i] & 0xff, i2b(ch[i] & 0xff, 1));
-		if (ch[i] < '!') {
-			printf("\n");
-		} else {
-			printf("\t%c\n", ch[i]);
-		}
+		print_bits(i, ch[i], 1);
 	}
 	return 0;
 }
