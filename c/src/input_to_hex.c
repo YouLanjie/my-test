17ade1e783c891f8253f4c4e4e6775aa7a06463a
YouLanjie <ylj18926385312@163.com>
2026-08-06 19:26:07 +0800

    修改input_to_hex.c支持参数设置最小长度，仅输出含utf8字符行，移除功能重复文件

--- c/src/input_to_hex.c
+++ c/src/input_to_hex.c
@@ -19,6 +19,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 static bool quiet = false;
 static bool flag_isatty = false;
@@ -44,22 +45,23 @@ static char *u8tbits(uint8_t data, char *buf, size_t buf_size)
 	return buf;
 }
 
-int print_bits(int ind, uint8_t ch)
+int bits2str(int ind, uint8_t ch, char *buffer, size_t buffer_size)
 {
 	static int len_need = 0, len = 0;
 	static char wchar[10] = "";
 	static bool space = false;
 	char buf[65] = "";
 	char *sep = "\t";
+	int ret = 0;
 
 	if (!quiet) {
-		printf("ch[%s%3d%s]: "
-		       "%s0x%s%02x%s: "
-		       "%s0b%s%s%s",
-		       colors[3], ind, colors[0],
-		       colors[2], colors[3], ch, colors[0],
-		       colors[2], colors[3],
-		       u8tbits(ch, buf, sizeof(buf)), colors[0]);
+		snprintf(buffer+strlen(buffer), buffer_size-strlen(buffer),
+			 "ch[%s%3d%s]: %s0x%s%02x%s: %s0b%s%s%s",
+			 colors[3], ind, colors[0],
+			 colors[2], colors[3], ch, colors[0],
+			 colors[2], colors[3],
+			 u8tbits(ch, buf, sizeof(buf)), colors[0]);
+		ret = 1;
 	} else sep = "";
 
 	/* TODO: fix the clang-tidy report (in normal way):
@@ -68,8 +70,10 @@ int print_bits(int ind, uint8_t ch)
 	for (int i = 0; i < 4; i++);
 
 	if (ch < INT8_MAX && isprint(ch)) {
-		printf("%s%s%c%s", sep, isspace(ch)&&!quiet?"<SPACE>'":"", ch,
-		       isspace(ch)?"'":"");
+		snprintf(buffer+strlen(buffer), buffer_size-strlen(buffer),
+			 "%s%s%c%s", sep,
+			 isspace(ch)&&!quiet?"<SPACE>'":"", ch,
+			 isspace(ch)?"'":"");
 		space = false;
 	} else if (ch & 0b10000000) {
 		wchar[len] = ch;
@@ -77,12 +81,15 @@ int print_bits(int ind, uint8_t ch)
 		if (!len_need) {
 			for (int i = 0; ch & (0b10000000>>i); i++) len_need = i+1;
 			if (len_need > 4) len_need = 1;
-			if (!quiet) printf("%s<UTF8-CHAR-LEN=%d>", sep, len_need);
+			if (!quiet)
+				snprintf(buffer+strlen(buffer), buffer_size-strlen(buffer),
+					 "%s<UTF8-CHAR-LEN=%d>", sep, len_need);
 		} else if ((ch & 0b11000000) != 0b10000000) {
 			len_need = 1;
 		}
 		if (len_need>1 && len == len_need) {
-			printf("%s%s", sep, wchar);
+			snprintf(buffer+strlen(buffer), buffer_size-strlen(buffer),
+				 "%s%s", sep, wchar);
 			memset(wchar, 0, sizeof(wchar));
 			space = false;
 		}
@@ -92,22 +99,57 @@ int print_bits(int ind, uint8_t ch)
 		}
 	} else {
 		if (quiet && !space) {
-			printf("\n");
+			ret = 1;
 			space = true;
 		}
-		if (len_need && !quiet) printf("%s<UTF8-DECODE-ERROR>", sep);
+		if (len_need && !quiet)
+			snprintf(buffer+strlen(buffer), buffer_size-strlen(buffer),
+				 "%s<UTF8-DECODE-ERROR>", sep);
 		len_need = 0;
 		len = 0;
 	}
-	if (!quiet) printf("\n");
-	return 0;
+	if (!quiet) ret = 1;
+	return ret;
 }
 
 int main(int argc, char *argv[])
 {
-	// for $(cat "$FILE"|input_to_hex -q|grep -E '^.{8,}$')
-	if (argc >= 2 && strcmp(argv[1], "-q") == 0) quiet = true;
-
+	int inp = 0;
+	size_t min_len = 1;
+	bool utf8_only = false;
+	char buf[PATH_MAX] = "";
+	while ((inp = getopt(argc, argv, "hf:n:qu")) != -1) {
+		switch (inp) {
+		case '?':
+		case 'h':
+			printf("Usage: ./%s [-hqu] [-n <min_len>] [-f <FILE>]\n",
+			       argc > 1 ? argv[0] : "input_to_hex");
+			return 0;
+			break;
+		case 'q':
+			quiet = true;
+			break;
+		case 'u':
+			utf8_only = true;
+			break;
+		case 'f':
+			strncpy(buf, optarg, sizeof(buf)-1);
+			break;
+		case 'n':
+			sscanf(optarg, "%zu", &min_len);
+			if (min_len <= 0) min_len = 1;
+			break;
+		}
+	}
+	if (buf[0]) {
+		int fd = open(buf, O_RDONLY);
+		if (fd < 0) {
+			perror("open");
+			return 1;
+		}
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
 	if (!isatty(STDOUT_FILENO)) {
 		for (size_t i = 0; i < countof(colors); i++) colors[i] = "";
 	}
@@ -123,11 +165,21 @@ int main(int argc, char *argv[])
 			if ((size = fread(ch, 1, sizeof(ch), stdin)) <= 0) break;
 		}
 		for (size_t i = 0; i < size && (!flag_isatty||ch[i] != 0); ++i) {
-			print_bits(i, ch[i]);
+			if (!bits2str(i, ch[i], buf, sizeof(buf)-1))
+				continue;
+			if (strlen(buf) < min_len) {
+				memset(buf, 0, sizeof(buf));
+				continue;
+			}
+			inp = 0;
+			while (utf8_only && buf[inp] && !(buf[inp]&0b10000000))
+				inp++;
+			if (!utf8_only || buf[inp]&0b10000000)
+				printf("%s\n", buf);
+			memset(buf, 0, sizeof(buf));
 		}
 		if (size < sizeof(ch)) break;
 	} while (!flag_isatty);
-	if (quiet) printf("\n");
 	return 0;
 }
 
